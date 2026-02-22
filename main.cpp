ab59e9a3e16926a150ccaca7fc023acb2b19f4df
Christine Susic <csusic022@insite.4cd.edu>
2026-02-21 21:08:51 -0800

    Commit

--- main.cpp
+++ main.cpp
@@ -1,33 +1,37 @@
 // COMSC-210 | Lab 12 | Christine Susic
 
 #include <iostream>
+#include <algorithm>
+#include <numeric> 
+#include <array>
 #include <fstream>
 using namespace std;
 
+const int SIZE = 30; //size of array
+
 int main() {
+    //array of ice cream
+    array<int, SIZE> iceCream = {2, 3};
     //temporary variables
     int tempNumber; //catalog number
-    string tempFlavor; //flavor of ice cream
     int tempScoops; //number of scoops
+    int tempToppings; //number of toppings
     
     //reading from file
     ifstream fin; 
     fin.open("records.txt");
-    /*if (fin.good()) {
-        cout << "File read successfully.";
-        fin.close();
-    }
-    else 
-        cout << "File not found.\n";*/
     
     //three records in the file (int, string, int)
     while (fin >> tempNumber) {  // reads int tempNumber
-        fin.ignore();  //needed between an int and string
-        getline(fin, tempFlavor);  //reads string tempFlavor
         fin >> tempScoops; //reads int tempScoops
-        cout << tempNumber << ": " << tempFlavor << endl;
-        cout << "Number of scoops: " << tempScoops << endl << endl;
+        fin >> tempToppings;  //reads string tempFlavor
+        cout << tempNumber << endl;
+        cout << tempScoops << endl;
+        cout << tempToppings << endl << endl;
     }
+    
+    //output array
+    for (int val : iceCream) cout << val << " "; cout << endl;
 
     
     return 0;
